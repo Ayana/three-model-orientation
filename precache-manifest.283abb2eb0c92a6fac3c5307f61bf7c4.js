2818d7dcbf5a77f90d5c41eba8730dfcb050b9a6
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-17 12:33:19 -0400

    Updates

---
+++ precache-manifest.283abb2eb0c92a6fac3c5307f61bf7c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ab0fe8dd249e244cc66060c1d6bc2c",
+    "url": "/Ayana/three-model-orientation/index.html"
+  },
+  {
+    "revision": "88f7999e6d3fb4782407",
+    "url": "/Ayana/three-model-orientation/static/css/main.8c00a45f.chunk.css"
+  },
+  {
+    "revision": "c8b05bd5c9fd98b897fd",
+    "url": "/Ayana/three-model-orientation/static/js/2.9b22c409.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Ayana/three-model-orientation/static/js/2.9b22c409.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88f7999e6d3fb4782407",
+    "url": "/Ayana/three-model-orientation/static/js/main.312461bd.chunk.js"
+  },
+  {
+    "revision": "782be293842825a3ea10",
+    "url": "/Ayana/three-model-orientation/static/js/runtime-main.422d06a3.js"
+  },
+  {
+    "revision": "dfebfcc1c20036036dd467bde0ed9f11",
+    "url": "/Ayana/three-model-orientation/static/media/model.dfebfcc1.obj"
+  },
+  {
+    "revision": "4ab88924a06aa9736f75ed82514216f7",
+    "url": "/Ayana/three-model-orientation/static/media/sky.4ab88924.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Ayana/three-model-orientation/precache-manifest.283abb2eb0c92a6fac3c5307f61bf7c4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ayana/three-model-orientation/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
